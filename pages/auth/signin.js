84b5876ffe114aac6661c1446e53ecdde1f39d12
Dotadotnet <Dotadotnet@outlook.com>
2025-03-12 02:09:37 +0330

    add phone login

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -2,6 +2,7 @@
 import Logo from "@/components/shared/logo/Logo";
 import ThemeToggle from "@/components/shared/navbar/ThemeToggle/ThemeToggle";
 import GoogleLogin from "./user/GoogleLogin";
+import PhoneLogin from "./user/phoneLogin/PhoneLogin";
 const signin = () => {
   return (
     <section
@@ -26,6 +27,7 @@ const signin = () => {
               یا
             </p>
           </div>
+          <PhoneLogin />
         </div>{" "}
         <div className="flex justify-center">
           <ThemeToggle />
